282f7c2c84df4381d95aca4c8cea5bc4a1f99211
vptsk <vassilis.pitsikalis@gmail.com>
2013-07-12 03:10:23 +0300

    modifying web.js to read file

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+buf = new Buffer(256);
+//len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+fs.readFileSync('index.html', function (err, buf) {
+  if (err) throw err;
+    console.log(data);
+    });
+len = buf.length;
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2 !');
+  response.send(len + " bytes: " + buf.toString('utf8', 0, len))
+  //response.send('Hello World 2 !');
 });
 
 var port = process.env.PORT || 5000;

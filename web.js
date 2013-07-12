0a6102849c32d51a449359edbf7f5a688f81338b
vptsk <vassilis.pitsikalis@gmail.com>
2013-07-12 04:02:38 +0300

    debug web.js (to read file)

--- web.js
+++ web.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
 var fs = require('fs');
-
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256);
+//var buf = new Buffer(256);
+////var json = JSON.stringify(buf.toString('utf8', 0 , len));
 
-fs.readFile('./index.html', function (err, buf) {
-  if (err) throw err;
-  //if (err) console.log('ERROR')
-    console.log(buf);
-});
-
-var len = buf.length;
 
 app.get('/', function(request, response) {
-  response.send(len + " bytes: " + buf.toString('utf8', 0, len))
-  //response.send('Hello World 2 !');
+	var buf = new Buffer(256);
+
+	fs.readFile('./index.html', function (err, buf) {
+		if (err) throw err;
+		console.log("READ OK >" + buf.toString('utf8', 0, buf.length) + " < END ");
+	});
+
+	//response.send(buf.toString('utf8', fs.readFileSync("./index.html")))
+	response.send(buf.toString('utf8', 0, buf.length))
 });
 
 var port = process.env.PORT || 5000;
